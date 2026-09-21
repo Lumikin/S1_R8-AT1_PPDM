0257a0b605b807fdd5d06fac76cef006b01a5bc2
Lumikin <lucas.m.ribeiro11@aluno.senai.br>
2026-09-21 12:41:51 -0300

    :image: feat: Adicionando os icones dos elementos e requisições da api

--- src/lib/pokeApi.js
+++ src/lib/pokeApi.js
@@ -12,6 +12,40 @@ async function ListarPokemons() {
 }
 
 async function ListarDadosPokemon(pokemon) {
-  const result = await API_POKEMON.get(`/pokemon/${pokemon}`);
-  return result;
+  try {
+    const api = await API_POKEMON.get(`/pokemon/${pokemon}`);
+
+    const dataPokemon = {
+      id: api.data.id,
+      nome: api.data.name,
+      altura: api.data.height / 10, // Transformar em metros
+      peso: api.data.weight / 10, // Transformar em KG
+      movimentos: api.data.moves.map(m => m.move.name), // Mostra todos os movimentos em uma array
+      imgMale: api.data.sprites.front_default,
+      imgMaleS: api.data.sprites.front_shiny,
+      imgFemale: api.data.sprites.front_female,
+      imgFemaleS: api.data.sprites.front_shiny_female,
+    };
+
+    return dataPokemon;
+  } catch (error) {
+    if (error.response?.status === 404) {
+      return null;
+    }
+    console.error(error);
+    return [];
+  }
+}
+const meuPokemon = await ListarDadosPokemon("zeraora");
+if (!meuPokemon) {
+  console.log("Não encontrado");
+} else {
+  console.log(meuPokemon.id);
+  console.log(`${meuPokemon.peso} KG`);
+  console.log(`${meuPokemon.altura} Metros`);
+  console.log(meuPokemon.movimentos);
+  console.log(`${meuPokemon.imgMale} Normal `);
+  console.log(`${meuPokemon.imgMaleS} Shiny `);
+  console.log(`${meuPokemon.imgFemale} Female`);
+  console.log(`${meuPokemon.imgFemaleS} Female Shiny`);
 }
